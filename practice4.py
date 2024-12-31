6279043d923d61f7c23672a97de55e3c2c008b2e
마상위 <ma0719@naver.com>
2024-12-31 10:45:26 +0900

    commit2

--- practice4.py
+++ practice4.py
@@ -1,1 +1,5 @@
 import pandas as pd
+df = pd.read_csv('C:/Users/마상위/Desktop/Data/iris.csv')
+df
+df.info()
+df.loc[3,'Petal_Width']
---
+++ project4.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv('C:/Users/마상위/Desktop/Data/GNI2014.csv')
+df
+df.head()
